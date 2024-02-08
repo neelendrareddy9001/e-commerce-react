834fc7555c4c8278efa90b8718e3428401de9519
neelendrareddy9001 <neelendrareddy123@gmail.com>
2024-02-08 18:18:26 +0100

    completed filter function of individuals

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useDebugValue } from "react";
 // import { items } from "./Data";
 import { Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Product = ({ items, cart, setCart }) => {
   const addToCart = ({ id, price, title, description, imgSrc }) => {
@@ -13,61 +15,74 @@ const Product = ({ items, cart, setCart }) => {
     };
     setCart([...cart, obj]);
     console.log("Cart Element", cart);
+    toast.success("Item added on Cart", {
+      position: "top-right",
+      autoClose: 1500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: dark,
+    });
   };
 
   return (
-    <div className="container my-5">
-      <div className="row">
-        {items.map((product) => {
-          return (
-            <>
-              <div
-                className="col-lg-4 col-md-6 com-sm-12 my-3 text-center"
-                key={product.id}
-              >
-                <div className="card " style={{ width: "18rem" }}>
-                  <Link
-                    to={`/product/${product.id}`}
-                    style={{
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }}
-                  >
-                    <img
-                      src={product.imgSrc}
-                      className="card-img-top"
-                      alt=".."
-                    />
-                  </Link>
-
-                  <div className="card-body ">
-                    <h5 className="card-title">{product.title}</h5>
-                    <p className="card-text">{product.description}</p>
-                    <button
-                      className="btn btn-primary mx-3"
-                      onClick={() =>
-                        addToCart(
-                          product.id,
-                          product.price,
-                          product.title,
-                          product.description,
-                          product.imgSrc
-                        )
-                      }
+    <>
+      <ToastContainer />
+      <div className="container my-5">
+        <div className="row">
+          {items.map((product) => {
+            return (
+              <>
+                <div
+                  className="col-lg-4 col-md-6 com-sm-12 my-3 text-center"
+                  key={product.id}
+                >
+                  <div className="card " style={{ width: "18rem" }}>
+                    <Link
+                      to={`/product/${product.id}`}
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
                     >
-                      {product.price}
-                      {""} ₹
-                    </button>
-                    <button className="btn btn-warning">Add To Cart</button>
+                      <img
+                        src={product.imgSrc}
+                        className="card-img-top"
+                        alt=".."
+                      />
+                    </Link>
+
+                    <div className="card-body ">
+                      <h5 className="card-title">{product.title}</h5>
+                      <p className="card-text">{product.description}</p>
+                      <button
+                        className="btn btn-primary mx-3"
+                        onClick={() =>
+                          addToCart(
+                            product.id,
+                            product.price,
+                            product.title,
+                            product.description,
+                            product.imgSrc
+                          )
+                        }
+                      >
+                        {product.price}
+                        {""} ₹
+                      </button>
+                      <button className="btn btn-warning">Add To Cart</button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </>
-          );
-        })}
+              </>
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/SearchItem.jsx
+++ src/components/SearchItem.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { items } from "./Data";
+import Product from "./Product";
 
 const SearchItem = () => {
   const { term } = useParams();
-  return <div>SearchItem - {term}</div>;
+  const [filterData, setFilterData] = useState([]);
+
+  useEffect(() => {
+    const filteredData = () => {
+      const data = items.filter((p) =>
+        p.title.toLowerCase().includes(term.toLowerCase())
+      );
+      setFilterData(data);
+    };
+    filteredData();
+  }, [term]);
+  return <Product items={filterData} />;
 };
 
 export default SearchItem;
