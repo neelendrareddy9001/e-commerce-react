78a42509f347f2203e06dd49986ae2c4cc609079
neelendrareddy9001 <neelendrareddy123@gmail.com>
2024-02-09 23:20:43 +0100

    add styles to cart componnet

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
-const Cart = () => {
-  return <div>Cart</div>;
+const Cart = ({ cart, setCart }) => {
+  return (
+    <>
+      <div className="container">
+        {cart.map((product) => {
+          return (
+            <>
+              <div className="card mb-3" style={{ width: "700px" }}>
+                <div className="row g-0">
+                  <img
+                    src={product.imgsrc}
+                    className="img-fluid rounded-start"
+                    alt=""
+                  ></img>
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h5 className="card-title">{product.title}</h5>
+                    <p className="card-text">{product.description}</p>
+                    <button className="btn btn-primary mx-3">
+                      {product.price}
+                    </button>
+                    <button className="btn btn-warning">Buy Now</button>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Cart;
